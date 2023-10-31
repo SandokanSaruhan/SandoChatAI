cdc600cbe0a10659a9bd7ca936d48c0d63f89fd1
Sandokan Saruhan KOLE <sandokankole@gmail.com>
2023-10-31 18:09:28 +0300

    the codes

---
+++ SandoChatAI/SandoChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandoChatAIApp.swift
+//  SandoChatAI
+//
+//  Created by Saruhan Kole on 3.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SandoChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
